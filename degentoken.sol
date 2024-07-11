b14e73994903c6772ff1c69f41cf03095d656928
Vaibhav Chhillar <131567808+VaibhavChhillar@users.noreply.github.com>
2024-07-11 15:44:52 +0530

    Update degentoken.sol

--- degentoken.sol
+++ degentoken.sol
@@ -6,67 +6,130 @@ Redeeming tokens: Players should be able to redeem their tokens for items in the
 Checking token balance: Players should be able to check their token balance at any time.
 Burning tokens: Anyone should be able to burn tokens, that they own, that are no longer needed.*/
 
-
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract DegenToken is ERC20, Ownable(msg.sender) {
-    constructor() ERC20("Degen", "DGN") {}
+contract DegenToken is IERC20{
+    string private _name;
+    string private _symbol;
+    address private _owner;
+    mapping(address => uint256) private _balances;
+    mapping(address => string[]) private _purchases; 
+    uint256 private _totalSupply = 0;
 
-    // Store item structure
-    struct StoreItem {
-        string itemName;
-        uint256 price;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    event Burn(address from, uint256 value);
+    event Mint(address to, uint256 value);
+
+    constructor(string memory name, string memory symbol) {
+        name = "Degen";
+        symbol = "DGN";
+        _owner = msg.sender;
     }
 
-    // Store items list
-    StoreItem[] public storeItems;
-
-    // Add store items (onlyOwner)
-    function addStoreItem(string memory itemName, uint256 price) external onlyOwner {
-        storeItems.push(StoreItem(itemName, price));
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
     }
 
-    // Mint new tokens and distribute them to players as rewards. Only the owner can do this.
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
     }
 
-    // Transfer tokens from the sender's account to another account.
-    function transferTokens(address to, uint256 amount) external {
-        require(balanceOf(msg.sender) >= amount, "Don't have enough Degen Tokens");
-        _transfer(msg.sender, to, amount);
+    function transfer(address to, uint256 value) public returns (bool) {
+        
+        if (to == address(0)) {
+            revert InvalidReceiver(address(0));
+        }
+        address sender = msg.sender;
+        uint256 senderBalance = _balances[sender]; 
+        if (_balances[sender] < value) {
+            revert InsufficientBalance(sender, senderBalance, value);
+        } 
+        _balances[sender] -= value;
+        _balances[to] += value;
+        emit Transfer(sender, to, value);
+        return true;
     }
 
-    // Redeem tokens for items in the in-game store based on the store item index.
-    event RedeemItem(address indexed player, string itemName, uint256 price);
 
-    function redeem(uint256 itemIndex) public virtual {
-        require(itemIndex < storeItems.length, "Invalid store item index");
-        uint256 price = storeItems[itemIndex].price;
-        require(balanceOf(msg.sender) >= price, "Not enough Degen Tokens to redeem this item");
-
-        _burn(msg.sender, price);
-        emit RedeemItem(msg.sender, storeItems[itemIndex].itemName, price);
+    function burn(uint256 value) public returns(bool) {
+        address sender = msg.sender;
+        uint256 senderBalance = _balances[sender];
+        if(_balances[sender] < value) {
+            revert InsufficientBalance(sender, senderBalance, value);
+        }
+        _balances[sender] -= value;
+        _totalSupply -= value;
+        emit Burn(sender, value);
+        return true;
     }
 
-    // Anyone can burn their own tokens that are no longer needed.
-    function burn(uint256 amount) public virtual {
-        _burn(msg.sender, amount);
+    function mint(address to, uint256 value) public returns(bool) {
+        require(msg.sender == _owner, "Only owner is allowed to perform this operation"); 
+        _balances[to] += value;
+        _totalSupply += value;
+        emit Mint(to, value);
+        return true;
     }
-}
 
-contract StoreInitializer {
-    // Deploy the DegenToken contract and add examples to storeItems
-    constructor() {
-        DegenToken token = new DegenToken();
-
-        // Add examples to storeItems
-        token.addStoreItem("Epic Sword of Valor", 100);
-        token.addStoreItem("Mystic Potion of Power", 50);
-        token.addStoreItem("Legendary Shield of Fortitude", 200);
+    function getPurchases(address account) public view returns(string[] memory) {
+        return _purchases[account];
     }
+
+    function redeem(string memory itemName, uint256 value) public{
+        address sender = msg.sender;
+        uint256 senderBalance = _balances[sender];
+        if(_balances[sender] < value) {
+            revert InsufficientBalance(sender, senderBalance, value);
+        }
+        _balances[sender] -= value;
+        _purchases[sender].push(itemName);
+        emit Transfer(sender, address(this), value);
+    }
+
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 value) external returns (bool) {
+        address owner = msg.sender;
+        uint256 ownerBalance = _balances[owner];
+        if (spender == address(0)) {
+            revert InvalidReceiver(spender);
+        }
+        if (ownerBalance < value) {
+            revert InsufficientBalance(owner, ownerBalance, value);
+        }
+        _balances[owner] -= value;
+        _allowances[owner][spender] += value;
+        emit Approval(owner, spender, value);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 value) external returns (bool) {
+        address spender = msg.sender;
+        uint256 allowanceBalance =  _allowances[from][spender];
+
+        if (to == address(0)) {
+            revert InvalidReceiver(to);
+        }
+        if (allowanceBalance < value) {
+            revert InsufficientAllowance(spender, from, allowanceBalance, value);
+        }
+        _allowances[from][spender] -= value;
+        _balances[to] += value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+
+    error InvalidReceiver(address _to);
+
+    error InsufficientBalance(address from,uint256 fromBalance,uint256 value);
+
+    error InsufficientAllowance(address spender, address from, uint256 currentAllowance, uint256 value);
+
 }
